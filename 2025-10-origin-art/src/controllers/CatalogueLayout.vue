a9789735ea522b6829ed3f0be5bb05f653c271e0
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-10-10 10:15:49 +0200

    1

---
+++ 2025-10-origin-art/src/controllers/CatalogueLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row no-wrap items-start">
+    <!-- 🧭 Sidebar -->
+    <div class="col-12 col-md-3 q-pr-md">
+      <div
+        class="q-pa-md bg-3ry-color text-white rounded-borders"
+        style="position: sticky; top: 80px; z-index: 2;"
+      >
+        <slot name="filters" />
+      </div>
+    </div>
+
+    <!-- 🖼️ Catalogue -->
+    <div class="col-12 col-md-9">
+      <slot name="content" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default { name: 'CatalogueLayout' }
+</script>
