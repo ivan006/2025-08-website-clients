86b7567417728da4234d1119d11e9ff96848c5a4
Ivan Copeland <ivan.copeland2015@gmail.com>
2026-02-04 14:00:24 +0200

    1

--- 2025-10-origin-art/src/models/orm-api/ArtistsBoundCache.js
+++ 2025-10-origin-art/src/models/orm-api/ArtistsBoundCache.js
@@ -1,23 +1,22 @@
-import BasicModel from './BasicModel'
+import BasicModel from "./BasicModel";
 
 export default class ArtistsBoundCache extends BasicModel {
-
-  static entity = 'ArtistsBoundCache';
-  static entityUrl = '/Artist';
+  static entity = "ArtistsBoundCache";
+  static entityUrl = "/Artist";
 
   static get airtableBaseUrl() {
-    return 'https://api.airtable.com/v0/appWL8gDT9ZaqV8jY'
+    return "https://api.airtable.com/v0/appWL8gDT9ZaqV8jY";
   }
-  
-  static get proxyBaseUrl() {
-    return `${import.meta.env.VITE_API_PROXY_URL}/cacher/data-cache/bound-cache.php?action=get&url=`
+
+  static proxyBaseUrl() {
+    return `${
+      import.meta.env.VITE_API_PROXY_URL
+    }/cacher/data-cache/bound-cache.php?action=get&url=`;
   }
 
   static defaultFlags = {
     view: "viwqEmrRk14mVdu4z",
-  }
-  
+  };
+
   // static entityUrl = '/Artwork?view=viwRVEM6ePmPsrZE4';
-
-
 }
--- 2025-10-origin-art/src/models/orm-api/ArtworksBoundCache.js
+++ 2025-10-origin-art/src/models/orm-api/ArtworksBoundCache.js
@@ -1,24 +1,22 @@
-import BasicModel from './BasicModel'
+import BasicModel from "./BasicModel";
 
 export default class ArtworksBoundCache extends BasicModel {
-
-  static entity = 'ArtworksBoundCache';
-  static entityUrl = '/Art';
+  static entity = "ArtworksBoundCache";
+  static entityUrl = "/Art";
 
   static get airtableBaseUrl() {
-    return 'https://api.airtable.com/v0/appWL8gDT9ZaqV8jY'
+    return "https://api.airtable.com/v0/appWL8gDT9ZaqV8jY";
   }
-  
-  static get proxyBaseUrl() {
-    return `${import.meta.env.VITE_API_PROXY_URL}/cacher/data-cache/bound-cache.php?action=get&url=`
+
+  static proxyBaseUrl() {
+    return `${
+      import.meta.env.VITE_API_PROXY_URL
+    }/cacher/data-cache/bound-cache.php?action=get&url=`;
   }
-  
 
   static defaultFlags = {
     // view: "viwn7wDGK6yk5ZHOl",
-  }
-  
+  };
+
   // static entityUrl = '/Artwork?view=viwRVEM6ePmPsrZE4';
-
-
 }
